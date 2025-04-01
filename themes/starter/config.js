07d1e297a3a251e68510ed17f58227325743189b
mu0hsh <fanxiang325@gmail.com>
2025-04-01 20:20:10 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -25,11 +25,13 @@ const CONFIG = {
   STARTER_HERO_BANNER_IMAGE: '', // hero区下方的全宽图
 
   // 顶部右侧导航暗流
+  /*
   STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
   STARTER_NAV_BUTTON_1_URL: '/sign-in',
 
   STARTER_NAV_BUTTON_2_TEXT: 'Sign Up',
   STARTER_NAV_BUTTON_2_URL: '/sign-up',
+  */
 
   // 特性区块
   STARTER_FEATURE_ENABLE: true, // 特性区块开关
